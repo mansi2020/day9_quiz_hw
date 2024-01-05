a46c23d002e9a1659381bc5473fdcee6d6a1e70b
mansi2020 <sorathiyamansi11@gmail.com>
2024-01-06 01:31:27 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import QuizMain from './Component/QuizMain/QuizMain';
+import { useState } from "react";
+import "./App.css";
+import QuizMain from "./Component/QuizMain/QuizMain";
+import quizImage from "./Component/quizHomeImage.svg";
 
 function App() {
+  let [showQuiz, setShowQuiz] = useState(false);
+  function onClickStartTest() {
+    setShowQuiz(true);
+  }
   return (
     <div className="App">
-      <h1>Quiz App</h1>
-      <QuizMain></QuizMain>
+      {showQuiz ? (
+        <QuizMain></QuizMain>
+      ) : (
+        <div className="app-content">
+          <img src={quizImage} alt="" />
+          <div className="app-content-sub">
+            <h1>QuizQuest: Explore, Learn, Play!</h1>
+
+            <button onClick={onClickStartTest}>Start Test</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/Component/QuizCard/QuizCard.js
+++ src/Component/QuizCard/QuizCard.js
@@ -1,50 +1,57 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from "react";
+import "./QuizCard.css";
 
 const QuizCard = (props) => {
-    // onClickBtn
-    let onClickBtn = (key,idx)=>{
-        props.onClickOption(key,idx);
-    }
-    // onClickSkip
-    let onClickSkip = ()=>{
-        props.onClickSkip();
-    }
+  // onClickBtn
+  let onClickBtn = (key, idx) => {
+    props.onClickOption(key, idx);
+  };
+  // onClickSkip
+  let onClickSkip = () => {
+    props.onClickSkip();
+  };
 
-    //useefect timer for every question
-    let [timer,setTimer] = useState(10);
-    useEffect(()=>{
-        let interval = setInterval(()=>{
-            setTimer((preVal) =>{
-                let updatedVal = preVal-1;
-                if(updatedVal == 0){
-                    props.goNextQuestion();
-                    clearInterval(interval);
-                }
-                return updatedVal;
-            });
-        },800);
-    },[])
-
-    
-  return (
-    <div className='quizcard-container'>
-      <h1>Question {props.questionNum}</h1>
-      <p>{props.question}</p>
-      <ul>
-        
-        {
-            Object.entries(props.answers).map(([key,value],idx)=>{
-                if(value != null){
-                    return  (<button  key={key} onClick={()=>onClickBtn(key,idx)} style={{display:"block"}}><li>{value}</li></button>)
-                } 
-               
-            })
+  //useefect timer for every question
+  let [timer, setTimer] = useState(10);
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setTimer((preVal) => {
+        let updatedVal = preVal - 1;
+        if (updatedVal == 0) {
+          props.goNextQuestion();
+          clearInterval(interval);
         }
-      </ul>
-        <p>Time Left: {timer} seconds</p>
-        <button onClick={onClickSkip}>Skip Question</button>
-    </div>
-  )
-}
+        return updatedVal;
+      });
+    }, 800);
+  }, []);
 
-export default QuizCard
+  return (
+    <section>
+      <div className="quizcard-container">
+        <h1>Question {props.questionNum}</h1>
+        <p className="quizcard-question">{props.question}</p>
+        <ul>
+          {Object.entries(props.answers).map(([key, value], idx) => {
+            if (value != null) {
+              return (
+                <button
+                  className="quizcard-options"
+                  key={key}
+                  onClick={() => onClickBtn(key, idx)}
+                  style={{ display: "block" }}
+                >
+                  <li>{value}</li>
+                </button>
+              );
+            }
+          })}
+        </ul>
+        <p className="quizcard_timer">Time Left: {timer} seconds</p>
+        <button onClick={onClickSkip}>Skip Question</button>
+      </div>
+    </section>
+  );
+};
+
+export default QuizCard;
